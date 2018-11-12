84f062130bfe57704b787765b041c0a53298793f
David May-Miller <David.May-Miller@softwire.com>
2018-11-12 14:58:21 +0000

    Rooks can now move anywhere

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,16 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var available = new List<Square>();
+            var location = board.FindPiece(this);
+            for (var row = 0; row < 8; row++)
+            {
+                for (var col = 0; col < 8; col++)
+                {
+                    available.Add(new Square(row,col));
+                }
+            }
+            return available;
         }
     }
 }
