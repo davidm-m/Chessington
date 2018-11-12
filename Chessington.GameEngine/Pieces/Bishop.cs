91fa931cc140ad8566b3e93ce5da75077b902f92
David May-Miller <David.May-Miller@softwire.com>
2018-11-12 15:16:10 +0000

    Bishops can now move diagonally

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,26 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var available = new List<Square>();
+            var location = board.FindPiece(this);
+            for (var i = 1; location.Row + i < 8 && location.Col + i < 8; i++)
+            {
+                available.Add(Square.At(location.Row + i, location.Col + i));
+            }
+            for (var i = 1; location.Row + i < 8 && location.Col - i >= 0; i++)
+            {
+                available.Add(Square.At(location.Row + i, location.Col - i));
+            }
+            for (var i = 1; location.Row - i >= 0 && location.Col + i < 8; i++)
+            {
+                available.Add(Square.At(location.Row - i, location.Col + i));
+            }
+            for (var i = 1; location.Row - i >= 0 && location.Col - i >= 0; i++)
+            {
+                available.Add(Square.At(location.Row - i, location.Col - i));
+            }
+
+            return available;
         }
     }
 }
